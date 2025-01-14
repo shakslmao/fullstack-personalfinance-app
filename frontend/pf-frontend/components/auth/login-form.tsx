0d50e16adf111d409a25f07d3c93dfa9b3a9385e
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-14 19:39:20 +0000

    new init frontend

---
+++ frontend/pf-frontend/components/auth/login-form.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const LoginForm = () => {
+    return (
+        <div>
+            <h1>hello login</h1>
+        </div>
+    );
+};
---
+++ frontend/pf-frontend/components/auth/register-form.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const RegisterForm = () => {
+    return (
+        <div>
+            <h1>hello users</h1>
+        </div>
+    );
+};
