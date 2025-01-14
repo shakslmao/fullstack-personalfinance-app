d5da0b8c1469224583c2caecb89f5812abcfb301
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-14 00:29:58 +0000

    page for login / register

---
+++ frontend/personal-finance-frontend/app/user/login/page.tsx
@@ -0,0 +1,6 @@
+import { LoginForm } from "@/components/user/login-form";
+
+const LoginPage = () => {
+    return <LoginForm />;
+};
+export default LoginPage;
---
+++ frontend/personal-finance-frontend/app/user/register/page.tsx
@@ -0,0 +1,7 @@
+import { RegisterForm } from "@/components/user/register-form";
+
+const RegisterPage = () => {
+    return <RegisterForm />;
+};
+
+export default RegisterPage;
