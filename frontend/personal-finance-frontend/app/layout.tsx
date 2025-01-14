ddf59ae1cf9dbadfb5d1c7a76d8cf6f9c8f9adc2
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-14 00:30:07 +0000

    root layout changes

--- frontend/personal-finance-frontend/app/layout.tsx
+++ frontend/personal-finance-frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -13,16 +14,24 @@ export const metadata: Metadata = {
     title: "Personal Finance",
     description: "Your Personal Finance Application",
 };
-
+// root layer
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en">
+        <html
+            lang="en"
+            suppressHydrationWarning>
             <body className={cn("relative h-full font-sans antialiased", geistSans)}>
-                <main className="relative flex flex-col min-h-screen">{children}</main>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange>
+                    <main className="relative flex flex-col min-h-screen">{children}</main>
+                </ThemeProvider>
             </body>
         </html>
     );
