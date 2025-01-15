c74938981ff16bb487166cdf996597cef65d2b67
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-15 18:42:28 +0000

    updated root layout to take keycloak provider

--- frontend/pf-frontend/app/layout.tsx
+++ frontend/pf-frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import { KeycloakProvider } from "@/context/keycloak-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,18 +18,20 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html
-            lang="en"
-            suppressHydrationWarning>
-            <body className={cn("relative h-full font-sans antialiased", inter.className)}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange>
-                    <main className="relative flex flex-col min-h-screen">{children}</main>
-                </ThemeProvider>
-            </body>
-        </html>
+        <KeycloakProvider>
+            <html
+                lang="en"
+                suppressHydrationWarning>
+                <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+                    <ThemeProvider
+                        attribute="class"
+                        defaultTheme="dark"
+                        enableSystem
+                        disableTransitionOnChange>
+                        <main className="relative flex flex-col min-h-screen">{children}</main>
+                    </ThemeProvider>
+                </body>
+            </html>
+        </KeycloakProvider>
     );
 }
