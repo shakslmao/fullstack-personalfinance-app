c084f5ed43306c2142288b13eaf388ddc37b45e8
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-01-16 15:40:09 +0000

    updates

--- frontend/pf-frontend/app/layout.tsx
+++ frontend/pf-frontend/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Figtree, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 import { KeycloakProvider } from "@/context/keycloak-provider";
 
 const inter = Inter({ subsets: ["latin"] });
+const figtree = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "FinFlow",
@@ -21,7 +22,7 @@ export default function RootLayout({
         <html
             lang="en"
             suppressHydrationWarning>
-            <body className={cn("relative h-full font-sans antialiased", inter.className)}>
+            <body className={cn("relative h-full font-sans antialiased", figtree.className)}>
                 <ThemeProvider
                     attribute="class"
                     defaultTheme="dark"
---
+++ frontend/pf-frontend/components/auth/reset-password-form.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const ResetPasswordForm = () => {
+    return (
+        <div>
+            <h1>reset</h1>
+        </div>
+    );
+};
