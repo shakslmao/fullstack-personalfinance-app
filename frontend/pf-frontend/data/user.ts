8e4aa7090ca656dd4c23dfb1c86b1943e790f59c
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-05 17:10:07 +0000

    updated method

--- frontend/pf-frontend/data/user.ts
+++ frontend/pf-frontend/data/user.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import api from "pages/api/api";
 import { User } from "types/auth";
 
 export const fetchUserDetails = async (userId: string | undefined): Promise<User | null> => {
@@ -8,31 +9,14 @@ export const fetchUserDetails = async (userId: string | undefined): Promise<User
         return null;
     }
 
-    const token = document.cookie
-        .split("; ")
-        .find((row) => row.startsWith("jwt="))
-        ?.split("=")[1];
-
-    if (!token) {
-        console.error("No authentication token found.");
-        return null;
-    }
-
     try {
-        const userDetailsResponse = await fetch(`http://localhost:8090/api/v1/users/${userId}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`, // ✅ Include JWT token
-            },
-        });
+        const { data } = await api.get<User>("/api/v1/users");
 
-        if (!userDetailsResponse.ok) {
+        if (!data) {
             throw new Error("Failed to fetch user details.");
         }
 
-        const userDetails = await userDetailsResponse.json();
-        return userDetails;
+        return data;
     } catch (error) {
         console.error("Error fetching user details:", error);
         return null;
