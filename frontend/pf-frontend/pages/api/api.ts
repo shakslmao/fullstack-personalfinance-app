a31aa17a8312b04a3de941eb6992d0b7fc39e7aa
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-05 15:26:36 +0000

    removed frontend auth/ handling all auth backend only

--- frontend/pf-frontend/pages/api/api.ts
+++ frontend/pf-frontend/pages/api/api.ts
@@ -3,12 +3,12 @@
 import axios from "axios";
 import { AuthenticationResponse, RegistrationResponse } from "../../types/auth";
 import {
+    TActivationTokenSchema,
     TLoginValidationSchema,
     TRegistrationValidationSchmea,
     TResetPasswordValidationSchema,
 } from "../../schemas";
-import { signIn } from "auth";
-import { LOGIN_REDIRECT } from "routes";
+import { LOGIN_REDIRECT, REGISTER_REDIRECT } from "routes";
 
 const NEXT_PUBLIC_API_AUTH_URL = process.env.NEXT_PUBLIC_API_AUTH_URL;
 const NEXT_PUBLIC_API_GATEWAY_URL = process.env.NEXT_PUBLIC_API_GATEWAY_URL;
@@ -45,11 +45,9 @@ export const login = async (
         const { data } = await api.post<AuthenticationResponse>("/api/v1/auth/login", loginDetails);
         console.log("Login Response:", data);
 
-        await signIn("credentials", {
-            email: loginDetails.email,
-            password: loginDetails.password,
-            redirectTo: LOGIN_REDIRECT,
-        });
+        if (data.token) {
+            document.cookie = `jwt=${data.token}; Path=/; Secure; HttpOnly`;
+        }
 
         return data;
     } catch (error) {
@@ -57,6 +55,18 @@ export const login = async (
         throw error;
     }
 };
+
+export const activate = async (activationCode: TActivationTokenSchema) => {
+    try {
+        const { data } = await api.post("/api/v1/auth/activate", activationCode);
+        console.log("activation response:", data);
+        return data;
+    } catch (err) {
+        console.log("Failed to Activate Account with Token");
+        throw err;
+    }
+};
+
 export const resetPassword = async (resetCredentials: TResetPasswordValidationSchema) => {
     const { data } = await gatewayApi.post("/api/v1/users/reset", resetCredentials);
     return data;
