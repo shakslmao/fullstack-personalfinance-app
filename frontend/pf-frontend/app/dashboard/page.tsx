88fd12e6343cb7b77e0d24242ea42aee5065afda
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-06 19:11:46 +0000

    basic dashboard to test auth and logins

--- frontend/pf-frontend/app/dashboard/page.tsx
+++ frontend/pf-frontend/app/dashboard/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 
 import { fetchUserDetails } from "data/user";
+import { useEffect, useState } from "react";
+import { User } from "types/user";
 
 const DashboardPage = () => {
-    return <div>i should only see this if im auth</div>;
+    const [user, setUser] = useState<User | null>(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchUserDetails()
+            .then((data) => {
+                setUser(data);
+                setLoading(false);
+            })
+            .catch(() => setLoading(false));
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (!user) return <div>You Must Be Logged In to Access This Page</div>;
+
+    return <div>Welcome, {user.firstname}</div>;
 };
 
 export default DashboardPage;
