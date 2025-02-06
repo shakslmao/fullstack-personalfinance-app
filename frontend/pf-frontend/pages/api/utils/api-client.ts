f309dc3a3ea2110d509512560a0b202f2f02b852
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-06 13:15:51 +0000

    updated api client

--- frontend/pf-frontend/pages/api/utils/api-client.ts
+++ frontend/pf-frontend/pages/api/utils/api-client.ts
@@ -1,12 +1,7 @@
 import axios from "axios";
-import {
-    TActivationTokenSchema,
-
-} from "../../../schemas";
 
 const NEXT_PUBLIC_API_AUTH_URL = process.env.NEXT_PUBLIC_API_AUTH_URL;
 const NEXT_PUBLIC_API_GATEWAY_URL = process.env.NEXT_PUBLIC_API_GATEWAY_URL;
-
 
 export const gatewayApi = axios.create({
     baseURL: NEXT_PUBLIC_API_GATEWAY_URL,
@@ -25,15 +20,3 @@ export const api = axios.create({
     },
     withCredentials: true,
 });
-
-export const activate = async (activationCode: TActivationTokenSchema) => {
-    try {
-        const { data } = await api.post("/api/v1/auth/activate", activationCode);
-        console.log("activation response:", data);
-        return data;
-    } catch (err) {
-        console.log("Failed to Activate Account with Token");
-        throw err;
-    }
-};
-
