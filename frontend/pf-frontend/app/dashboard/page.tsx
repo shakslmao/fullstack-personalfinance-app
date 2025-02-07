6cfe1b2903f3e6f4353e4604c7b0f4d6bc182004
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-07 22:23:50 +0000

    moved dashboard logic page to component/user

--- frontend/pf-frontend/app/dashboard/page.tsx
+++ frontend/pf-frontend/app/dashboard/page.tsx
@@ -1,34 +1,7 @@
-"use client";
-
-import LogoutButton from "components/auth/logout-button";
-import { fetchUserDetails } from "data/user";
-import { useEffect, useState } from "react";
-import { User } from "types/user";
+import { Dashboard } from "components/user/dashboard-page";
 
 const DashboardPage = () => {
-    const [user, setUser] = useState<User | null>(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        fetchUserDetails()
-            .then((data) => {
-                setUser(data);
-                setLoading(false);
-            })
-            .catch(() => setLoading(false));
-    }, []);
-
-    if (loading) return <div>Loading...</div>;
-    if (!user) return <div>You Must Be Logged In to Access This Page</div>;
-
-    return (
-        <div>
-            <div>Welcome, {user.firstname}</div>
-            <div>
-                <LogoutButton />
-            </div>
-        </div>
-    );
+    return <Dashboard />;
 };
 
 export default DashboardPage;
