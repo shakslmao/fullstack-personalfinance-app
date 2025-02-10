eb54e61ed6542a4584965d28cc3d26d0da7b15bd
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-10 18:09:39 +0000

    minor ui change

--- frontend/pf-frontend/components/formError.tsx
+++ frontend/pf-frontend/components/formError.tsx
@@ -6,7 +6,7 @@ export const FormMessagingError = ({ errorMessage }: FormMessagingErrorProps) =>
     if (!errorMessage) return null;
 
     return (
-        <div className="bg-white p-4 rounded-md flex justify-center gap-x-2 text-center text-sm text-white">
+        <div className="bg-white-500/10 p-4 rounded-md flex justify-center gap-x-2 text-center text-sm text-white">
             {errorMessage}
         </div>
     );
