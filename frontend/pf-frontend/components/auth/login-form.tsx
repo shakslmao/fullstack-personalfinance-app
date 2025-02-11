234ee3af0dd90a096bf114c5df0319746ccc1eda
shaakir akram <shaakir2.akram@live.uwe.ac.uk>
2025-02-11 18:29:17 +0000

    updated login form so that users can request a new auth token if they try to login without one

--- frontend/pf-frontend/components/auth/login-form.tsx
+++ frontend/pf-frontend/components/auth/login-form.tsx
@@ -20,18 +20,25 @@ import { Button, buttonVariants } from "../ui/button";
 import Link from "next/link";
 import { Separator } from "../ui/separator";
 import { ArrowRight } from "lucide-react";
-import { LoginValidationSchema, TLoginValidationSchema } from "schemas";
+import {
+    LoginValidationSchema,
+    ResetTokenEmailValidationSchema,
+    TLoginValidationSchema,
+    TResetTokenEmailValidationSchema,
+} from "schemas";
 import { useRouter } from "next/navigation";
 import { LOGIN_REDIRECT } from "routes";
 import { AuthenticationResponse } from "types/auth";
 
-// For Non Auth Attempting To Login, Make them Reqeust a new Token on Account Login Page.
+// For Non Auth Attempting To Login, Make them Reqeust a new Token on Account Login Page. & disbale login button
 
 export const LoginForm = () => {
     const router = useRouter();
     const [isSubmitting, startTransition] = useTransition();
+    const [isSubmittingTokenReset, startTransitionTokenReset] = useTransition();
     const [validationError, setValidationError] = useState<string | undefined>("");
     const [validationSuccess, setValidationSucceess] = useState<string | undefined>("");
+    const [showActivationForm, setShowActivationForm] = useState(false);
 
     const form = useForm<TLoginValidationSchema>({
         resolver: zodResolver(LoginValidationSchema),
@@ -41,9 +48,15 @@ export const LoginForm = () => {
         },
     });
 
+    const emailForm = useForm<TResetTokenEmailValidationSchema>({
+        resolver: zodResolver(ResetTokenEmailValidationSchema),
+        defaultValues: { email: "" },
+    });
+
     const onSubmit = (data: TLoginValidationSchema) => {
         setValidationError("");
         setValidationSucceess("");
+        setShowActivationForm(false);
 
         startTransition(async () => {
             try {
@@ -62,6 +75,7 @@ export const LoginForm = () => {
                         setValidationError("Incorrect Email or Password");
                     } else if (response.status === 401) {
                         setValidationError("You need to authenticate your account.");
+                        setShowActivationForm(true);
                     } else if (response.status === 403) {
                         setValidationError("Your account is not authorised to proceed.");
                     } else {
@@ -77,12 +91,43 @@ export const LoginForm = () => {
         });
     };
 
+    const onSubmitAuthenticate = (data: TResetTokenEmailValidationSchema) => {
+        setValidationError("");
+        setValidationSucceess("");
+
+        console.log("Submitting Reset Token Request with email:", data.email);
+
+        startTransitionTokenReset(async () => {
+            try {
+                if (!data.email) {
+                    setValidationError("Email is required to request new activation code");
+                    return;
+                }
+
+                const response = await fetch("/api/auth/request-new-token", {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ email: data.email }),
+                });
+
+                const result = await response.json();
+                if (!response.ok) throw new Error(result.error);
+
+                setValidationSucceess("A new activation token has been sent to your email.");
+                setShowActivationForm(false);
+
+                setTimeout(() => router.push("/auth/activate"), 2000);
+            } catch (error) {
+                setValidationError("Failed to send a new activation token.");
+            }
+        });
+    };
+
     return (
         <>
             <div className="flex items-center justify-center min-h-screen mx-auto">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <div className="flex flex-col items-center space-y-2 text-center">
-                        {/* <!-- Logo -->  className="h-20 w-20"*/}
                         <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
                             Hello Again!
                             <br />
@@ -94,7 +139,6 @@ export const LoginForm = () => {
                             <form
                                 className="space-y-4"
                                 onSubmit={form.handleSubmit(onSubmit)}>
-                                {/* Username input field */}
                                 <div className="space-y-2">
                                     <FormField
                                         control={form.control}
@@ -107,7 +151,7 @@ export const LoginForm = () => {
                                                 <FormControl>
                                                     <Input
                                                         {...field}
-                                                        disabled={isSubmitting} // Disable input while submitting
+                                                        disabled={isSubmitting}
                                                         placeholder="Enter your Email"
                                                         type="email"
                                                     />
@@ -117,7 +161,6 @@ export const LoginForm = () => {
                                         )}
                                     />
                                 </div>
-                                {/* Password input field */}
                                 <div className="space-y-2">
                                     <FormField
                                         control={form.control}
@@ -130,7 +173,7 @@ export const LoginForm = () => {
                                                 <FormControl>
                                                     <Input
                                                         {...field}
-                                                        disabled={isSubmitting} // Disable input while submitting
+                                                        disabled={isSubmitting}
                                                         placeholder="Enter your Password"
                                                         type="password"
                                                     />
@@ -142,15 +185,61 @@ export const LoginForm = () => {
                                 </div>
                                 <FormMessagingError errorMessage={validationError} />
                                 <FormMessagingSuccess successMessage={validationSuccess} />
-                                {/* Submission  field */}
-                                <Button
-                                    disabled={isSubmitting} // Disable button while submitting
-                                    className="mt-auto py-4 m-auto w-full text-center"
-                                    type="submit">
-                                    Login
-                                </Button>
+
+                                {!showActivationForm ? (
+                                    <Button
+                                        disabled={isSubmittingTokenReset}
+                                        className="mt-auto py-4 m-auto w-full text-center"
+                                        type="submit">
+                                        Login
+                                    </Button>
+                                ) : null}
                             </form>
                         </Form>
+                        {showActivationForm && (
+                            <Form {...emailForm}>
+                                <form
+                                    className="space-y-6"
+                                    onSubmit={emailForm.handleSubmit(onSubmitAuthenticate)}>
+                                    <div className="flex flex-col space-y-4">
+                                        <FormField
+                                            control={emailForm.control}
+                                            name="email"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel className="block text-medium text-white">
+                                                        Email
+                                                    </FormLabel>
+                                                    <FormControl>
+                                                        <Input
+                                                            {...field}
+                                                            disabled={isSubmittingTokenReset}
+                                                            placeholder="Enter your email"
+                                                            type="email"
+                                                            className="text-center px-4 py-3 border border-gray-300 rounded-md"
+                                                        />
+                                                    </FormControl>
+                                                    <FormMessage className="text-center" />
+                                                </FormItem>
+                                            )}
+                                        />
+                                        <div className="mt-4">
+                                            <Button
+                                                className={buttonVariants({
+                                                    variant: "default",
+                                                    className:
+                                                        "w-full py-4 bg-white rounded-lg text-black font-semibold",
+                                                    size: "sm",
+                                                })}
+                                                disabled={isSubmittingTokenReset}
+                                                type="submit">
+                                                Send New Code
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </Form>
+                        )}
                         <p className="text-sm max-w-prose text-center text-muted-foreground text-white">
                             <Link
                                 href="/auth/reset"
